e8c7e18f4d65339cd38f2e65ce03750779155835
Akif Bugday <akif@backbase.com>
2022-12-29 15:36:18 +0100

    test

--- scripts/node-version.js
+++ scripts/node-version.js
@@ -1,10 +1,10 @@
 const { exec } = require('child_process');
 const { run } = require('nx/src/command-line/run');
 
-function run(){
+function runScript(){
     console.log('akif')
     const {stdout} = exec('node -v')
     console.log(`stdout:\n${stdout}`);
 }
 
-run()
+runScript()
